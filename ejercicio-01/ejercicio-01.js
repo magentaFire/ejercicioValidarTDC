32a1e4d0dd4e9e17bcd5ae6b56d0dbbc6ca7483b
magentaFire <dafne.magenta@gmail.com>
2017-04-10 02:40:34 -0500

    Agreagdos algunos casos

--- ejercicio-01/ejercicio-01.js
+++ ejercicio-01/ejercicio-01.js
@@ -1,8 +1,27 @@
 function isValidCard(creditCardNumber) {
     // Escribe tu código aquí
-    
+var pares;
+var impares;
+
+if (typeof creditCardNumber == "string") {
+  return "Error de dato"
+} else if (typeof creditCardNumber == "boolean") {
+  return "Error de dato"
+} else if (typeof creditCardNumber == "object") {
+  return "Error de dato"
+} else if (typeof creditCardNumber != "number") {
+  return "Ingresa un numero"
+} else if (creditCardNumber.toString().length < 16) {
+  return "Faltan numeros"
 }
 
+var stringNumber = creditCardNumber.toString();
+stringNumber.filter(function(num){
+
+  return
+});
+
+}
 // NO TOCAR ESTE CÓDIGO O EXPLOTARÁ LA PC EN 10 SEGUNDOS
 if (typeof exports !== 'undefined') {
     exports.isValidCard = isValidCard;
